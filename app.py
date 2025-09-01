9fa6d7497be6073092c88066f8204476eea00d10
lpisk <lpiskorski96@gmail.com>
2025-09-01 13:32:58 +0200

    zmiana linku

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+    
+@app.route('/demo-motobot.html')
+def demo_motobot():
+    return render_template('demo-motobot.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
